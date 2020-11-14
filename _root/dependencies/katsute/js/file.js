d4e86f702b33176f9aab58606b0c15967da6f96c
Katsute <58778985+Katsute@users.noreply.github.com>
2020-11-13 22:03:36 -0500

    context menu changes

--- _root/dependencies/katsute/js/file.js
+++ _root/dependencies/katsute/js/file.js
@@ -6,15 +6,51 @@ $(document).ready(function () {
         return false;
     });
 
-    $(".file-link").on("contextmenu", function(e){
-        // todo: add js for dropdown menu
-        return false;
-    });
+    $(".file-link").contextmenu(show);
+    $("#files").contextmenu(show);
 
+    $("#files").scroll(hide);
     $(document).click(hide);
+    $(document).contextmenu(hide);
     $(window).blur(hide);
+    $(window).resize(hide)
 });
 
+var selected;
+
+const menu   = $("#context-menu");
+
+function show(e){
+    e.stopPropagation();
+    
+    var y      = e.pageY;
+    var x      = e.pageX;
+    var width  = menu.width();
+    var height = menu.height();
+
+    var top    = (y + height) > window.innerHeight ? y - height : y;
+    var left   = (x + width)  > window.innerWidth  ? x - width : x;
+
+    selected   = $(e.target).attr("file-target");
+    var dir    = $(e.target).attr("file-dir");
+
+    console.log(e.target);
+
+    $("#context-menu").find(".dropdown-item").each(function(_, btn){
+        var file_dir = $(btn).attr("file-dir");
+        if(file_dir != null && file_dir != dir)
+            $(btn).addClass("disabled");
+        else
+            $(btn).removeClass("disabled");
+    });
+
+    menu.css({
+        top: top,
+        left: left
+    }).addClass("d-block");
+    return false;
+}
+
 function hide(e){
-    // only if already open
+    $("#context-menu").removeClass("d-block");
 }
